b9a992dd9ddd465abc5fe95dac1fd29f9c235f9a
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-04 09:41:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,9 +19,22 @@ resource "azurerm_data_factory_pipeline" "demo_pipeline" {
     "name": "WaitActivity",
     "type": "Wait",
     "typeProperties": {
-      "waitTimeInSeconds": 10
+      "waitTimeInSeconds": 15
     }
   }
 ]
 JSON
+}
+
+resource "azurerm_data_factory_trigger_schedule" "demo_trigger" {
+  name            = "demo-trigger"
+  data_factory_id = azurerm_data_factory.adf.id
+
+  pipeline_name = azurerm_data_factory_pipeline.demo_pipeline.name
+
+  frequency = "Minute"
+  interval  = 5
+
+  start_time = "2025-01-01T00:00:00Z"
+  activated  = true
 }
