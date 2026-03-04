5e2cd38581d79bce91cd0a1cdd625528748fbf7c
bagewadiarti-hub <bagewadi.arti@gmail.com>
2026-03-04 09:15:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+resource "azurerm_data_factory" "adf" {
+  name                = var.adf_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
